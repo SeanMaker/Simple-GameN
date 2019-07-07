7385ab85e48d6be565a1eb01f70b6e667fe951a0
Shiheng Dong <dongshiheng9@gmail.com>
2019-07-06 21:54:28 -0400

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Simple-Game';
+  oddNumners: number[] = [];
+  evenNumbers: number[] = [];
+  OnIntervalFired(firedNumber: number){
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    }
+    else {
+      this.oddNumners.push(firedNumber);
+    }
+  }
+}
